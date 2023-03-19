7e9d7a28477277b46734eef2453fd5fe8f9aec1a
walter <aswinp2008@gmail.com>
2023-03-19 19:23:26 +0530

    general vim settings

---
+++ lua/walter/init.lua
@@ -0,0 +1,17 @@
+vim.o.encoding = 'utf-8'
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.mouse = 'a'
+vim.o.nu = true
+vim.o.rnu = true
+vim.o.cursorline = true
+vim.o.cursorlineopt = 'line'
+vim.o.scl = 'yes:1' --signs column
+vim.o.scrolloff = 5 --keep five lines in context always
+vim.o.wrap = false
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+vim.o.termguicolors = true
