f110443385797349e0d2a885ce2544b3a961809a
Aswin P Ajayan <aswinp2008@gmail.com>
2025-05-05 17:07:20 +0530

    cleanup

--- lua/walter/pckr.lua
+++ lua/walter/pckr.lua
@@ -1,20 +1,3 @@
-local function bootstrap_pckr()
-  local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
-
-  if not vim.loop.fs_stat(pckr_path) then
-    vim.fn.system({
-      'git',
-      'clone',
-      "--filter=blob:none",
-      'https://github.com/lewis6991/pckr.nvim',
-      pckr_path
-    })
-  end
-
-  vim.opt.rtp:prepend(pckr_path)
-end
-
-bootstrap_pckr()
 
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
